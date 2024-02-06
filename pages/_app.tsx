f27f75410d49c12abfa4b1ff9ce2e77e6d79f531
truewayy <dldudwls888@gmail.com>
2024-02-06 15:36:48 +0900

    feat: react query provider 및 hydrate 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,32 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
+import { useState } from 'react';
+import { Hydrate, QueryClient, QueryClientProvider } from 'react-query';
 
 import Header from '@/components/Header';
 import { colorTheme } from '@/public/theme/theme';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            refetchOnWindowFocus: false,
+            cacheTime: 1000 * 60 * 5,
+            staleTime: 1000 * 60 * 5,
+          },
+        },
+      })
+  );
   return (
-    <ChakraProvider theme={colorTheme}>
-      <Header />
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <ChakraProvider theme={colorTheme}>
+          <Header />
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </Hydrate>
+    </QueryClientProvider>
   );
 }
