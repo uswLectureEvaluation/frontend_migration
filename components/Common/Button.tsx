487dc40727222f8e2df2b71ed29217dcf25f56e9
truewayy <dldudwls888@gmail.com>
2024-02-12 18:23:09 +0900

    design: suwiki button 컴포넌트 disabled 속성일때 디자인 수정

--- components/Common/Button.tsx
+++ components/Common/Button.tsx
@@ -13,6 +13,15 @@ const SuwikiButton = ({ children, ...props }: SuwikiButtonProps) => (
     color="standard.white"
     fontWeight="500"
     borderRadius="15px"
+    _disabled={{
+      cursor: 'not-allowed',
+      opacity: 0.4,
+      _hover: {
+        cursor: 'not-allowed',
+        bg: 'main.blue',
+        opacity: 0.4,
+      },
+    }}
     {...props}
   >
     {children}
